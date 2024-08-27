711dfe3f7a13e954904da2ed9fe3d73ac5b928d7
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-27 16:12:30 +0700

    Lấy ra chi tiết bài báo

--- resolvers.ts
+++ resolvers.ts
@@ -5,12 +5,22 @@ export const resolvers = {
         hello: () => {
             return "Hello World!"
         },
-        
+
         getListArticle: async () => {
             const articles = await Article.find({
                 deleted: false
             })
             return articles
+        },
+
+        getArticle: async (_, args) => {
+            const { id } = args
+
+            const article = await Article.findOne({
+                _id: id, 
+                deleted: false
+            })
+            return article
         }
     }
 }
