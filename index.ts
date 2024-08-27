b7b4e515aa05000c515dcc257ded15b052e5e61f
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-27 15:55:11 +0700

    Cài đặt và sử dụng GraphQL trong project

--- index.ts
+++ index.ts
@@ -2,24 +2,33 @@ import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
 
 import * as database from "./config/database"
-import Article from "./models/article.model"
+import { ApolloServer } from "apollo-server-express"
+import { typeDefs } from "./typeDefs"
+import { resolvers } from "./resolvers"
 
-dotenv.config()
-database.connect()
+const startServer = async () => {
+    dotenv.config()
+    database.connect()
 
-const app: Express = express()
-const port: number | string = process.env.PORT || 3000
+    const app: Express = express()
+    const port: number | string = process.env.PORT || 3000
 
-app.get("/articles", async (req: Request, res: Response) => {
-    const articles = await Article.find({
-        deleted: false
+    // GraphQL
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
     })
 
-    res.json({
-        articles: articles
-    })
-})
+    await apolloServer.start()
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+    apolloServer.applyMiddleware({
+        app: app,
+        path: "/graphql"
+    })
+
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+}
+
+startServer()
